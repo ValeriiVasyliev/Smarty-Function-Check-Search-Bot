1a512d7144b57370e6df62ce4a1b59fe0a3d5d55
Valerii Vasyliev <info@gratta.com.ua>
2017-09-24 01:07:40 +0300

    first commit

---
+++ function.is_secured_request.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+ 
+
+/**
+ * smarty_function_is_secured_request()
+ * 
+ * @param mixed $params
+ * @param mixed $smarty
+ * @return
+ */
+ 
+/*
+    {is_secured_request assign="is_secured_request_status"}
+    
+    {is_secured_request ip = "127.0.0.1" agent = "google" assign="is_secured_request_status"}
+    
+    {if $is_secured_request_status}
+
+    {/if}
+*/
+function smarty_function_is_secured_request($params, &$smarty) {
+    
+    function is_bot_ip($ip) {
+
+	       $is_search_bot_ip=false;
+	
+           $hostname = gethostbyaddr($ip); //"crawl-66-249-66-1.googlebot.com"
+
+           if (preg_match('/\.googlebot|google\.com$/i', $hostname)) {
+		          $is_search_bot_ip=true;
+	       }
+	
+           if (preg_match('/\.yandexbot|yandex\.com$/i', $hostname)) {
+		          $is_search_bot_ip=true;
+	       }
+	
+	       return $is_search_bot_ip;
+    }
+    
+    
+    if(is_null($params['ip'])){
+
+        $ip=$_SERVER['REMOTE_ADDR'];
+    }
+
+    if(is_null($params['agent'])){
+
+        $agent=$_SERVER['HTTP_USER_AGENT'];
+    }
+
+    $is_bot_request=false;
+
+    if (strpos($agent, 'Google')!==false || is_bot_ip($ip) || strpos($agent, 'Yandex')!==false) {
+
+        $is_bot_request=true;
+    }
+    
+    if (!empty($params['assign'])) {
+        $smarty->assign($params['assign'],$is_bot_request);
+    } else {
+        return $is_bot_request;
+    }
+}
